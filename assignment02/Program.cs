ee3de2525a8cab1d0b064a24fed5264b50a47465
ahmed <ahmed@DESKTOP-LSUT3NK>
2024-12-17 03:30:37 +0300

    Add project files.

---
+++ assignment02/Program.cs
@@ -0,0 +1,331 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01
+            //1- Write a program that takes a number from the user then print yes
+            //if that number can be divided by 3 and 4 otherwise print no.
+
+            //Console.WriteLine("enternumber:");
+            //int Number = int.Parse(Console.ReadLine());
+            //if (Number % 3 == 0 && Number % 4 == 0)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+            #endregion
+            #region Q2
+            ////2- Write a program that allows the user to insert an integer then print negative
+            //if it is negative number otherwise print positive.
+
+
+            //Console.WriteLine("Enter Number");
+            //int Number=int.Parse(Console.ReadLine());
+            //if (Number < 0)
+            //{
+            //    Console.WriteLine("negative");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("positive");
+            //}
+
+
+
+
+
+
+
+            #endregion
+            #region Q3
+            //3 - Write a program that takes 3 integers from the user then
+            //prints the max element and the min element.
+            //Console.WriteLine("Enter Number");
+            //int Number01=int.Parse(Console.ReadLine());
+            //int Number02 =int.Parse(Console.ReadLine());
+            //int Number03=int.Parse(Console.ReadLine());
+            //int Max;
+            //int Min;
+
+            //if(Number01>Number02)
+            //{
+            //    Max=Number01;
+            //}
+            //else
+            //{
+            //    Max=Number02;
+            //}
+            //if(Number03>Max)
+            //{
+            //    Max=Number03;
+            //}
+            //Console.WriteLine("max:"+Max);
+            //if(Number01<Number02)
+            //{
+            //    Min = Number01;
+            //}
+            //else
+            //{
+            //    Min = Number02;
+            //}
+            //if (Number03 < Min)
+            //{
+            //    Min = Number03;
+            //}
+            //Console.WriteLine("MIN:"+Min);
+            #endregion
+            #region Q4
+            //Write a program that allows the user to insert an integer number then
+            //check If a number is even or odd.
+
+            //Console.WriteLine("enter number:");
+            //int Number=int.Parse(Console.ReadLine());   
+            //if(Number %2==0)
+            //{
+            //    Console.WriteLine("even");
+            //}
+            //else if(Number %2!=0)
+            //{
+            //    Console.WriteLine("odd");
+            //}
+
+
+
+
+            #endregion
+            #region Q5
+            //5- Write a program that takes character from the user then
+            //if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+
+            //Console.WriteLine("enter char");
+            //string Char=Console.ReadLine();
+            //if (Char=="a" ||Char == "e" ||Char == "i" || Char == "o" || Char == "u")
+
+            //{ 
+            //    Console.WriteLine("vowel");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("consonant");
+            //}
+
+
+            #endregion
+            #region Q6
+
+            // 6 - Write a program that allows the user to insert an integer
+            // then print all numbers between 1 to that number.
+
+            //Console.WriteLine("enter number");
+            //int Number=int.Parse(Console.ReadLine());
+            //for (int i = 1;i <= Number;i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+
+
+            #endregion
+            #region Q7
+            //7- Write a program that allows the user to insert an integer then 
+            //print a multiplication table up to 12.
+
+            //Console.WriteLine("enter number");
+            //int Number = int.Parse(Console.ReadLine());
+            //int i = 1;
+
+            //for (; i <= 12;)
+            //{
+            //    string Message = $"{Number} * {i} = {Number * i}";
+            //    Console.WriteLine(Message);
+            //    ; i++;
+            //}
+
+
+            //Console.WriteLine("enter number");
+            //int Number = int.Parse(Console.ReadLine());
+            //int i = 1;
+
+            //for (; i <= 12;)
+            //{
+            //    int Message = Number * i;
+            //    Console.WriteLine(Message);
+            //    ; i++;
+            //}
+
+
+
+
+
+            #endregion
+            #region Q8
+
+            // Write a program that allows to user to insert number
+            // then print all even numbers between 1 to this number
+
+            //Console.WriteLine("ENTER NUMBER");
+            //int Number = int.Parse(Console.ReadLine());
+            //for (int i = 2; i < Number; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+            #endregion
+            #region Q9
+            //Write a program that takes two integers then prints the power.
+
+
+            //Console.WriteLine("enter nimber");
+            //int Number01=int.Parse(Console.ReadLine());
+            //int Number02=int.Parse(Console.ReadLine());
+            //double Message = Math.Pow( Number01 , Number02);
+            //Console.WriteLine(Message);
+
+
+
+
+            #endregion
+            #region Q10
+            //10- Write a program to enter marks of five subjects and
+            //calculate total, average and percentage.
+
+
+            //int Subject01, Subject02, Subject03, Subject04, Subject05;
+
+            //Console.WriteLine("enter subject1:");
+            // Subject01 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter subject2:");
+            // Subject02 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter subject3:");
+            // Subject03 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter subject4:");
+            // Subject04 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("enter subject5:");
+            // Subject05 = int.Parse(Console.ReadLine());
+
+            //int Total = Subject01 + Subject02 + Subject03 + Subject04 + Subject05;
+            //double Avg = Total / 5.0;
+            //double Per = (Total / 5.0) * 100;
+
+            //Console.WriteLine("average:" + Avg);
+            //Console.WriteLine("percentage:" + Per);
+
+
+
+
+
+
+
+            #endregion
+            #region Q11
+            //11- Write a program to input the month number and print the number of days in that month.
+
+
+            //Console.WriteLine("MONTHNUMBER:");
+            //int Month=int.Parse(Console.ReadLine());
+            //switch (Month)
+            //{
+            //    case 1:
+            //    case 3:
+            //    case 5:
+            //    case 7:
+            //    case 8:
+            //    case 10:
+            //    case 12:
+            //        Console.WriteLine("32");
+            //        break;
+
+            //    case 4:
+            //    case 6:
+            //    case 9:
+            //    case 11:
+            //        Console.WriteLine("30");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("28");
+            //        break;
+            //}
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+            #region Q12
+            //Write a program to create a Simple Calculator.
+            //Console.WriteLine("CALCULATR");
+            //double Num1 = double.Parse(Console.ReadLine());
+            //double Num2= double.Parse(Console.ReadLine());
+            //double Choice;
+            //double Result ;
+            //switch(Choice)
+            //{
+            //    case 1: 
+            //        Result = Num1 + Num2;
+            //        Console.WriteLine(Result);
+            //        break;  
+
+            //}
+            #endregion
+            #region Q13
+            //Write a program to allow the user to enter a string and print the REVERSE of it.
+
+            //Console.WriteLine("enter word");
+            //string Word=Console.ReadLine();
+
+
+
+
+
+            #endregion
+            #region Q14
+            //14- Write a program to allow the user to enter int and print the REVERSED of it.
+
+
+
+
+
+
+            #endregion
+            #region Q15
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ demo/Program.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Channels;
+
+namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region STRING FORMATING
+            //1-STRING COCATINTION
+            //int X = 4;
+            //int Y = 5;
+            //string Message = "EQUTION:" + X + "+" + Y + "=" + (X + Y);
+
+
+            //STRING FORMAT>>>BUILT IN FUNCTION
+            //string Message= string.Format("EQUATION:{0}+{1}={2}",X,Y,(X+Y));
+
+            //Console.WriteLine(Message);
+
+            ////STRING ENTRPOLUTION($)
+
+            //string Message = &"EQUATION:{X}+{Y}={X=Y}";            
+            //    Console.WriteLine(Message);
+
+
+            #endregion
+            #region CONTROLSTATMENT
+            //CONDITONAL CONTROL (IF /SWITCH)
+            //LOOP CONTROL(FOR  /FROEACH /WHILE /DO-WHILE)
+
+
+
+
+            //SYNTAX(IF CONDITION)
+            //IF(EXPRETION OR CONDITION)
+            //{
+
+            //CODE
+
+            //}    
+            //EX
+
+            //Console.Write("ENTER NUMBER OF MONTH IN THE 1ST QUARTER:");
+            //int MonthNumber = int.Parse(Console.ReadLine());
+
+            //if (MonthNumber == 1)
+            //{
+            //    Console.WriteLine("jan");
+            //}
+            //if (MonthNumber == 2)
+            //{
+
+            //    Console.WriteLine("mar");
+            //}
+            //if (MonthNumber == 3)
+            //{
+            //    Console.WriteLine("mar");
+            //}
+
+
+            //SWITCH(EXPRITTION)
+            //switch(MonthNumber)
+            //    {
+            //    case 1:
+            //        Console.WriteLine("jan");
+            //        break;
+            //        case 2:
+            //        Console.WriteLine("feb");
+            //        break;
+            //        case 3:
+            //        Console.WriteLine("mar");
+            //        break;
+            //        default:
+            //        Console.WriteLine("nomonth");
+            //        break;
+            //    }
+
+            //ex
+
+            //Console.Write("enter your name:");
+            //string Name = Console.ReadLine();
+            //if (Name == "Ahmed"||Name=="ahmed")
+            //{
+            //    Console.WriteLine("hi Ahmed");
+            //}
+            //else if (Name == "Ali"||Name=="ali")
+            //{
+            //    Console.WriteLine("hi ali");
+            //}
+            //else if (Name == "Mohamed"||Name=="mohamed")
+            //{
+            //    Console.WriteLine("hi mohamed");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no name");
+            //}
+
+
+            ////with switch
+            //switch (Name)
+            //{
+            //    case "Ahmed":
+            //    case "ahmed":
+            //        Console.WriteLine("hi ahmed");
+            //        break;
+            //    case "ali":
+            //    case "Ali":
+            //        Console.WriteLine("hi ali");
+            //        break;  
+            //    case "marim":
+            //    case "Marim":
+            //        Console.WriteLine("hi marim");
+            //        break;
+            //        default:
+            //        Console.WriteLine("no name");
+            //        break;
+
+
+            //}
+
+            //ex
+            //Console.WriteLine("enter your age");
+            //int Age=int.Parse(Console.ReadLine());
+            //if (Age>18)
+            //{
+            //    Console.WriteLine("welcom");
+            //}
+            //else if (Age<18)
+            //{
+            //    Console.WriteLine("bye bye");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("equal");
+            //}
+
+            //using switch
+            //Console.WriteLine("enter your age");
+            //int Age=int.Parse(Console.ReadLine());
+            //switch(Age)
+            //{
+            //    case > 18:
+            //        Console.WriteLine("welcom");
+            //        break;
+            //    case < 18:
+            //        Console.WriteLine("bye bye");
+            //        break;
+            //        default:
+            //        Console.WriteLine("equal");
+            //        break;
+
+            //}
+
+
+
+
+
+            #endregion
+            #region EVALIOTION IN SWITCH
+            //IN C#7.0
+            // CAN MAKE PATERN MATCHING MAKE CONDITION 
+            //EX
+            //object Object= new object();
+            //Object = "Ahmed";
+            ////Object = 1;
+            //switch(Object)
+            //{
+            //    case int i when i > 0:
+            //        Console.WriteLine("int object");
+            //        break;
+            //    case string i when i.Length > 4:
+            //        Console.WriteLine("string object");
+            //        break;
+            //}
+
+
+            //IN C# 8.0
+            // (Syntax SUGER)
+            //string Input = Console.ReadLine();
+            //string Output = Input switch
+            //{
+            //    "1" => "option1",
+            //    "2" => "option2",
+            //    "3" => "option3",
+            //};
+            //  Console.WriteLine(Output); 
+            //string Input = Console.ReadLine();
+            // Console.WriteLine( Input switch
+            //{
+            //    "1" => "option1",
+            //    "2" => "option2",
+            //    "3" => "option3",
+            //});
+            //Console.WriteLine(Output);
+
+            #endregion
+            #region LOOP CONTROL STATMENT
+            //LOOP COTROL(FOR / FOREACH / WHILE / DO-WHILE )
+            //USED FOR REPEATION
+            //SYNTAX FOR 
+            //FOR(STATMENT,EXPRISSION,STATMENT)
+            //{
+            //CODE
+            //}
+            //Console.WriteLine("HELLO WORLD");
+            //int i = 0;
+            //for (; i <= 10;)
+            //{
+            //    Console.WriteLine("hello world");
+            //    i++;
+            //}
+
+
+            //for (int i = 0; i <= 10;  i++)
+            //{
+            //    Console.WriteLine("hello world");
+
+            //}
+
+            //FOREACH>>>USED WITH ARY(CONTINER)
+            //int[] Number = { 1, 2, 3,4,5,6 };
+            //foreach (int i in Number)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //using FOR
+            //int[] Number = { 1, 2, 3, 4, 5, 6 };
+            //for (int i = 0; i < Number.Length;i++)
+            //{
+            //    Console.WriteLine(Number[i]);
+            //}
+
+
+            //WHILE THE SAME BUT SYNTAX DIFARNCE
+            //WHILE(EXPRISSION)>>BOOL (TRUE OR FALSE)
+            //{
+            //CODE
+            //}
+            //int i = 0;
+            //  while (i<10)
+
+            //{
+            //    Console.WriteLine("HI");
+            //    i++;
+            //}
+
+
+            //DO-WHILE >>>SAME BUT DIFFERNT SYNTAX AND THE CODE EXCUTE FIRST
+            //DO
+            //{
+            //CODE
+            //}WHILE (EXPRIDSSION);
+
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine("hi");
+            //    i++;
+
+            //} while (i <= 10);
+            ////  //// //////////////////////////////////
+
+            //Console.WriteLine("even number");
+            //using do-while
+            //int Number;
+            //do
+            //{
+            //    Console.WriteLine("even");
+            //    Number = int.Parse(Console.ReadLine());
+
+
+            //} while (Number % 2!=0);
+            //Console.WriteLine(Number);
+            ////////////////////////////////////////
+            //IF USER(USER STRING)
+            //int Number;
+            //bool Flag;
+            //do
+            //{
+            //    Console.WriteLine("even number");
+            //    Flag = int.TryParse(Console.ReadLine(), out Number);
+
+            //} while (Number % 2!= 0||Flag==false);
+            //Console.WriteLine(Number);
+
+            #endregion
+            #region String
+            //STRING(IMMUTABLE>>CAN NOT CHANGE ITS VALUE AFTER CREATION)
+            //string Name;
+
+            //Name = "Ahmed";
+            //Console.WriteLine(Name.GetHashCode());
+            //Name = "ali";
+            //Console.WriteLine(Name.GetHashCode());
+            //Name = "mariam";
+            //Console.WriteLine(Name.GetHashCode());
+            #endregion
+            #region STRING BUILDER
+
+
+
+            #endregion
+
+
+
+
+
+
+        }
+    }
+}
+   
+
+
+
+
+
+                  
